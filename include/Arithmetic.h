e358700f1bdf50f01ee932b20cfd964d430155b2
Joshua Weinstein <jweinstein@splunk.com>
2017-10-17 22:00:21 +0700

    more arithmetic macros

--- include/Arithmetic.h
+++ include/Arithmetic.h
@@ -3,6 +3,8 @@
 
 #include "Token.h"
 
+//Arithmetic macros
+// @note both operands must be pointers to tokens
 #define Token_ADD(t1, t2) do { \
                 if(t1->type == t2->type) { \
                         switch(t1->type) { \
@@ -12,6 +14,17 @@
                         } \
                 } \
 } while(0)
+
+#define Token_SUB(t1, t2) do { \
+                if(t1->type == TokenType_Number && t2->type == TokenType_Number) \
+                        t1->val.number -= t2->val.number; \
+} while(0)
+
+#define Token_MUL(t1, t2) do { \
+                if(t1->type == TokenType_Number && t2->type == TokenType_Number) \
+                        t1->val.number *= t2->val.number; \
+} while(0)
+
 
 void Token_add(Token* t1, Token* t2);
 
