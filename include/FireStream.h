695dff9a208dedd22e5fbb7cc06a71e4fa178f5b
Joshua Weinstein <jweinstein@splunk.com>
2017-10-23 10:16:28 -0700

    new folder, stream work

--- include/FireStream.h
+++ include/FireStream.h
@@ -24,25 +24,37 @@
                 stream->itemEnd = stream->items + stream->len; \
 } while(0)
 
-//macro for writing some pointer and its type to the stream
-#define FireStream_WRITE(stream, value, n) do { \
-                if(stream->itemEnd != stream->end) { \
+//macro for writing some pointer without a specified type to the stream
+#define FireStream_WRITE_N(stream, value, n) do { \
+                if(n < (stream->cap - stream->len)) { \
                         memcpy(stream->itemEnd, value, n); \
                         stream->len += n; \
                         stream->itemEnd = stream->items + stream->len; \
-                } \
-} while(0)
+                } else { \
+                        FireStream_EXPAND(stream, (stream->cap + n * 2)); \
+                        memcpy(stream->itemEnd, value, n); \
+                        stream->len += n; \
+                        stream->itemEnd = stream->items + stream->len; \
+                } while(0)
 
 //expression for the amount of space remaining in the stream
-#define FireStream_SPACE(stream, type) ((stream->cap * sizeof(type)) - (stream->len * sizeof(type)))
+#define FireStream_SPACE_T(stream, type) ((stream->cap * sizeof(type)) - (stream->len * sizeof(type)))
 
-#define FireStream_LENGTH(stream, type) (stream->len / sizeof(type))
+#define FireStream_LENGTH_T(stream, type) (stream->len / sizeof(type))
 
 //casts the buffer of the stream toward relevant type
-#define FireStream_ITEMS(stream, type) ((type*)stream->items)
+#define FireStream_ITEMS_T(stream, type) ((type*)stream->items)
 
 //gets an item in the stream with specified type
-#define FireStream_GET(stream, type, ind) ((type*)stream->items)[0]
+#define FireStream_GET_T(stream, type, ind) ((type*)stream->items)[ind]
+
+//writes a typed value pointer to stream
+#define FireStream_WRITE_T(stream, type, value) do { \
+                if(sizeof(type) > (stream->cap - stream->len)) FireStream_EXPAND(stream, (stream->cap + sizeof(type) * 2)); \
+                *((type*)stream->itemEnd) = *value; \
+                stream->len += sizeof(type); \
+                stream->itemEnd = stream->items + stream->len; \
+} while(0)
 
 
 
@@ -65,7 +77,5 @@ struct FireStream
 };
 
 typedef FireStream FireStream;
-
-void FireStream_map();
 
 #endif
