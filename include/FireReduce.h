15594032fad25396d861d91fb212043948f6b6bc
Joshua Weinstein <jweinstein@splunk.com>
2017-10-29 00:14:16 -0700

    Arithmatic reduce done

--- include/FireReduce.h
+++ include/FireReduce.h
@@ -3,6 +3,7 @@
 //header that defines reducing macros
 
 #include "FireTypes.h"
+#include "Utils.h"
 
 #define FireReduce_ADD(stream) do { \
                 void* reductionReader = stream->items; \
@@ -14,6 +15,57 @@
                                 if(*(unsigned char*)reductionReader == FireStream_TYPE_NUM) { \
                                         reductionReader++; \
                                         *(double*)reductant += *(double*)reductionReader; \
+                                        reductionReader += sizeof(double); \
+                                } \
+                        } \
+                        stream->itemEnd = reductant + sizeof(double); \
+                } \
+} while(0)
+
+#define FireReduce_SUB(stream) do { \
+                void* reductionReader = stream->items; \
+                void* reductant = stream->items; \
+                if(*(unsigned char*)reductant == FireStream_TYPE_NUM) { \
+                        reductionReader += sizeof(double) + 1; \
+                        reductant++; \
+                        while (stream->itemEnd != reductionReader) { \
+                                if(*(unsigned char*)reductionReader == FireStream_TYPE_NUM) { \
+                                        reductionReader++; \
+                                        *(double*)reductant -= *(double*)reductionReader; \
+                                        reductionReader += sizeof(double); \
+                                } \
+                        } \
+                        stream->itemEnd = reductant + sizeof(double); \
+                } \
+} while(0)
+
+#define FireReduce_MUL(stream) do { \
+                void* reductionReader = stream->items; \
+                void* reductant = stream->items; \
+                if(*(unsigned char*)reductant == FireStream_TYPE_NUM) { \
+                        reductionReader += sizeof(double) + 1; \
+                        reductant++; \
+                        while (stream->itemEnd != reductionReader) { \
+                                if(*(unsigned char*)reductionReader == FireStream_TYPE_NUM) { \
+                                        reductionReader++; \
+                                        *(double*)reductant *= *(double*)reductionReader; \
+                                        reductionReader += sizeof(double); \
+                                } \
+                        } \
+                        stream->itemEnd = reductant + sizeof(double); \
+                } \
+} while(0)
+
+#define FireReduce_DIV(stream) do { \
+                void* reductionReader = stream->items; \
+                void* reductant = stream->items; \
+                if(*(unsigned char*)reductant == FireStream_TYPE_NUM) { \
+                        reductionReader += sizeof(double) + 1; \
+                        reductant++; \
+                        while (stream->itemEnd != reductionReader) { \
+                                if(*(unsigned char*)reductionReader == FireStream_TYPE_NUM) { \
+                                        reductionReader++; \
+                                        *(double*)reductant /= Utils_NO_ZERO(*(double*)reductionReader); \
                                         reductionReader += sizeof(double); \
                                 } \
                         } \
--- src/global/Main.c
+++ src/global/Main.c
@@ -7,7 +7,7 @@ int main( int argc, char *argv[] )
         FireStream* fstPtr = &fst;
         FireStream_MAKE_ZRNG(fstPtr, 10);
         printf(" len is then %lu\n", FireStream_LEN(fstPtr));
-        FireReduce_ADD(fstPtr);
+        FireReduce_MUL(fstPtr);
         FireMap_PRINT(fstPtr);
         return 0;
 }
