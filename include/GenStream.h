7276246615425d6c33581119ba03a6ab2c59f1e2
Joshua Weinstein <jweinstein@splunk.com>
2017-11-02 18:44:22 -0700

    base instruction set

--- include/GenStream.h
+++ include/GenStream.h
@@ -1,6 +1,12 @@
 #ifndef FIRE_GENSTREAM_H
 #define FIRE_GENSTREAM_H
-
 //generating file for stream instructions/segments, e.g [1 to 5]
+
+#include <stdlib.h>
+#include "ByteBuffer.h"
+
+
+
+void GenStream_gen(char* srcCode, ByteBuffer* bbuf);
 
 #endif
--- include/Instructions.h
+++ include/Instructions.h
@@ -2,6 +2,7 @@
 #define FIRE_INSTRUCTIONS_H
 // this header contains instruction codes that are named
 
+// WARNING max instruction is 255
 enum Instruction
 {
         Instruction_Null,
@@ -9,7 +10,25 @@ enum Instruction
         Instruction_Reduce_Add,
         Instruction_Reduce_Sub,
         Instruction_Reduce_Mul,
-        Instruction_Reduce_Div
+        Instruction_Reduce_Div,
+        Instruction_Push_Number,
+        Instruction_Push_ZRange,
+        Instruction_Map_Add,
+        Instruction_Map_Sub,
+        Instruction_Map_Mul,
+        Instruction_Map_Div,
+        Instruction_Map_Rem,
+        Instruction_Map_Floor,
+        Instruction_Map_Sin,
+        Instruction_Map_Cos,
+        Instruction_Map_Log,
+        Instruction_Map_Exp,
+        Instruction_Filt_Eq,
+        Instruction_Filt_Neq,
+        Instruction_Filt_Lt,
+        Instruction_Filt_Gt,
+        Instruction_Set_Zrange
+
 };
 
 typedef enum Instruction Instruction;
