848be7797301e51d64bc9f97c51872921e12db9f
Joshua Weinstein <jweinstein@splunk.com>
2017-11-06 15:52:58 -0800

    help function complete

--- src/global/Compile.c
+++ src/global/Compile.c
@@ -65,24 +65,53 @@ void Compile_repl()
 
 void Compile_help()
 {
-        puts("The Fire Programming Language Guide");
+        puts("\nThe Fire Programming Language Guide");
         puts("-----------------------------------");
         puts("Fire is a reactive, stream based language, you program");
         puts("in a data flow consisting of operations and ->'s.\n");
         puts("** Printing Data **");
+        puts("-----------------------------------");
         puts("@ out: prints the current stream and it's data.");
         puts("- ex: push 5 -> push 1 -> out\n result: [5 1 ]\n");
         puts("** Adding Data **");
+        puts("-----------------------------------");
         puts("@ push (number): pushes one number onto end of stream.");
         puts("- ex: push 5 -> out\n result: [5 ]");
         puts("@ rng (n): pushes a stream of numbers from zero to n onto the stream.");
         puts("- ex: rng 4 -> out\n result: [0 1 2 3 ]\n");
         puts("** Reducing Data **");
+        puts("-----------------------------------");
         puts("@ {+}: Sums all the numbers in the stream and store the result at the front.");
         puts("- ex: rng 4 -> {+} -> out\n result: [6 ]");
         puts("@ {-}: Subtracts all the numbers in the stream and store the result at the front.");
         puts("- ex:");
         puts("fire> rng 4 -> out -> {-} -> out\n[0 1 2 3 ]\n[-6 ]");
         puts("@ {*}: Multiplies all the numbers in the stream and store the result at the front.");
-        puts("@ {/}: Divides all the numbers in the stream and store the result at the front.");
+        puts("@ {/}: Divides all the numbers in the stream and store the result at the front.\n");
+        puts("** Mapping Data **");
+        puts("-----------------------------------");
+        puts("@ + (number): Adds some number to all numbers in the stream.");
+        puts("- ex:");
+        puts("fire> rng 4 -> out -> + 2 -> out\n[0 1 2 3 ]\n[2 3 4 5 ]");
+        puts("@ - (number): Subtracts some number from all numbers in the stream.");
+        puts("@ * (number): Multiplies each number in the stream.");
+        puts("@ / (number): Divides each number in the stream.");
+        puts("@ % (number): Calculates the remainder of each number of the stream by a number.");
+        puts("@ floor: Calculates and maps the floor of each number in the stream");
+        puts("- ex:");
+        puts("fire> rng 6 -> sin -> out -> floor -> out\n[0 0.841471 0.909297 0.141120 -0.756802 -0.958924 ]\n[0 0 0 0 -1 -1 ]");
+        puts("@ cos: Calculates and maps the cos of each number in the stream");
+        puts("@ sin: Calculates and maps the sin of each number in the stream");
+        puts("@ log: Calculates and maps the log(e) of each number in the stream");
+        puts("@ exp: Calculates and maps the (e)exp power of each number in the stream\n");
+        puts("** Filtering Data **");
+        puts("-----------------------------------");
+        puts("All filtering expressions in Fire begin with an 'if' keyword.\n Any element in the stream which holds false for the expression is removed from the string.\n");
+        puts("@ if == (number): Filters out all numbers not equal to (number) in the stream.");
+        puts("@ if != (number): Filters out all numbers equal to (number) in the stream.");
+        puts("@ if > (number): Filters out all numbers greater than (number) in the stream.");
+        puts("- ex:");
+        puts("fire> rng 20 -> if > 5 -> out\n[6 7 8 9 10 11 12 13 14 15 16 17 18 19 ]");
+        puts("@ if < (number): Filters out all numbers less than (number) in the stream.");
+
 }
