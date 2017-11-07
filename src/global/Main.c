e93834f19632bc9d7c474753571c75a3b7501589
Joshua Weinstein <jweinstein@splunk.com>
2017-11-06 16:19:19 -0800

    main fixed to guide if incorrect args

--- src/global/Main.c
+++ src/global/Main.c
@@ -8,6 +8,17 @@ int main( int argc, char *argv[] )
         {
                 if(!strcmp("-i", argv[1])) Compile_repl();
                 else if(!strcmp("-h", argv[1])) Compile_help();
+                else
+                {
+                        puts("Invalid command line arguments.");
+                        puts("-i : Starts the interactive REPL.");
+                        puts("-h : Opens the help guide.")
+                }
         }
-        else puts("Invalid command line arguments.");
+        else
+        {
+                puts("Invalid command line arguments.");
+                puts("-i : Starts the interactive REPL.");
+                puts("-h : Opens the help guide.")
+        }
 }
