253c78a8c15736aa7fee65cf3bbf56dcd42a3d9e
Joshua Weinstein <jweinstein@splunk.com>
2017-11-14 15:40:55 -0800

    implemented bytepack utils

---
+++ include/ByteWrite.h
@@ -0,0 +1,30 @@
+#ifndef BYTE_WRITE_H
+#define BYTE_WRITE_H
+//utility header for byte writing and patterning
+
+#include <stdlib.h>
+
+//struct type to facilitate typed byte regions
+struct ByteMark
+{
+        size_t block;
+        unsigned char mark;
+};
+
+typedef struct ByteMark ByteMark;
+
+//writes bytes from src to dest in size of block, with mark at the beginning of each block
+void ByteWrite_mark(void* restrict dest,
+                    void* restrict src,
+                    size_t block,
+                    unsigned char mark,
+                    int amount);
+
+// same as mark but keeps writing same src buffer over and over
+void ByteWrite_rep(void* restrict dest,
+                   void* restrict src,
+                   size_t block,
+                   unsigned char mark,
+                   int amount);
+
+#endif
